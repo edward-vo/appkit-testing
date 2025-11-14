91812f1ddc039e1901802d6bd35d1c35827afeee
edward <hello@edwardvo.ca>
2025-11-15 08:12:26 +0900

    add crash button

--- app/original/page.tsx
+++ app/original/page.tsx
@@ -21,10 +21,10 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <button onClick={disconnect} className="p-1 border cursor-pointer">Click to crash</button> <br/> <br/>
       iframe <br/>
       {navigator.userAgent}<br />
       {!window.ethereum ? 'No Eth' : 'Yes Eth'}<br />
-      {!window.ReactNativeWebView ? 'No WebV' : 'Yes WebV'}
     </div>
   );
 }
